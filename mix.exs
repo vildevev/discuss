3438050b1f180fa73669f7d10d50c2e23d02a6b3
“Vilde <vilde@vevatne.no>
2017-07-29 11:18:57 -0700

    Add ueberauth as dependency

--- mix.exs
+++ mix.exs
@@ -18,8 +18,9 @@ defmodule Discuss.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Discuss, []},
+      # tells Phoenix to initialize ueberauth and its extensions
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :ueberauth, :ueberauth_github]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +38,10 @@ defmodule Discuss.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     # Add ueberauth and github extension as dependencies
+     {:ueberauth, "~> 0.3"},
+     {:ueberauth_github, "~> 0.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
